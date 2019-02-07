960fe56aeeb07e64eec2c7929e67dabe14fc04c9
ibnahmad <justforlearningcode@gmail.com>
2019-02-07 13:08:00 +0100

    Without Java8 Filter

---
+++ src/StreamsFilterCollect/BeforeJava8.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StreamsFilterCollect;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author ibnahmad
+ */
+public class BeforeJava8 {
+    
+    public static void main(String[] args){
+        List<String> lines = Arrays.asList("spring", "node", "mkyong");
+        List<String> result = getFilterOutput(lines, "mkyong");
+        for(String temp : result){
+            System.out.println(temp);   //output : spring, node
+        }
+    }
+    
+    private static List<String> getFilterOutput(List<String> lines, String filter){
+       List<String> result = new ArrayList<>();
+       for(String line : lines){
+           if (!"mkyong".equals(line)){ // we dont like mkyong               
+               result.add(line);
+           }
+       }
+       return result;
+    }
+    
+}

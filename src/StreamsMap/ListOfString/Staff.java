3b67f47b2fae5528a0dd4162bde16b6dfbfc671a
ibnahmad <justforlearningcode@gmail.com>
2019-02-11 12:23:30 +0100

    List of String

---
+++ src/StreamsMap/ListOfString/Staff.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StreamsMap.ListOfString;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author ibnahmad
+ */
+public class Staff {
+    
+    private String name;
+    private int age;
+    private BigDecimal salary;
+
+    public Staff(String name, int age, BigDecimal salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+    
+    
+    
+}

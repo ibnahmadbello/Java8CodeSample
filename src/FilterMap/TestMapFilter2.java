ac33437def85541d400e201754d2696c7e4edf53
ibnahmad <justforlearningcode@gmail.com>
2019-02-12 17:51:07 +0100

    Filter Map

---
+++ src/FilterMap/TestMapFilter2.java
@@ -0,0 +1,36 @@
+package FilterMap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author ibnahmad
+ */
+public class TestMapFilter2 {
+    public static void main(String[] args){
+        
+        Map<Integer, String> HOSTING = new HashMap<>();
+        HOSTING.put(1, "linode.com");
+        HOSTING.put(2, "heroku.com");
+        HOSTING.put(3, "digitalocean.com");
+        HOSTING.put(4, "aws.amazon.com");              
+        
+        
+        //Map -> Stream -> Filter -> Map
+        Map<Integer, String> collect = HOSTING.entrySet().stream()
+                .filter(map -> map.getKey() == 2)                
+                .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
+        
+        System.out.println(collect);    // output : {heroku.com}
+                
+        
+        Map<Integer, String> collect2 = HOSTING.entrySet().stream()
+                .filter(map -> map.getKey() <= 3)                
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        
+        System.out.println(collect2);   // output : {1=linode.com, 2=heroku.com, 3=digitalocean.com}
+        
+    }
+}

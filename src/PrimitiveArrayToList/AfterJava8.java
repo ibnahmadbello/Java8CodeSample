1805e81292e0af8e16f10e72d84c8cb8121854f0
ibnahmad <justforlearningcode@gmail.com>
2019-02-12 12:13:11 +0100

    Primitive Array to List

---
+++ src/PrimitiveArrayToList/AfterJava8.java
@@ -0,0 +1,23 @@
+package PrimitiveArrayToList;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author ibnahmad
+ */
+public class AfterJava8 {
+    
+    public static void main(String[] args){
+        
+        int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        // IntStream.of or Arrays.stream, same output
+        // List<Integer> list = IntStream.of(number).boxed().collect(Collectors.toList());
+        
+        List<Integer> list = Arrays.stream(number).boxed().collect(Collectors.toList());
+        System.out.println("list : " + list);
+    }
+}
---
+++ src/PrimitiveArrayToList/BeforeJava8.java
@@ -0,0 +1,28 @@
+package PrimitiveArrayToList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author ibnahmad
+ */
+public class BeforeJava8 {
+    
+    public static void main(String[] args){
+        
+        int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        
+        List<Integer> list = convertIntArrayToList(number);
+        System.out.println("list : " + list);
+    }
+    
+    private static List<Integer> convertIntArrayToList(int[] input){
+        List<Integer> list = new ArrayList<>();
+        for (int i : input) {
+            list.add(i);
+        }
+        
+        return list;
+    }
+}
